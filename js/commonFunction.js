a759d19470115f56f32323c4db3235fe9edf0cd7
omor45faruk <omor45faruk@gmail.com>
2024-09-25 17:31:14 +0600

    added history part

---
+++ js/commonFunction.js
@@ -0,0 +1,9 @@
+function showSectionById(id){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
+
+
+
--- js/style.js
+++ js/style.js
@@ -1,35 +1,54 @@
 
+// donation /history btn
+document.getElementById('Donation').addEventListener('click',function(){
+    console.log('show add money btn');
+    showSectionById('donation-section');
+})
+
+document.getElementById('history').addEventListener('click',function(){
+    console.log('show add money btn');
+    showSectionById('history-section');
+})
 // card-1
 
-document.getElementById ('card-1').
-addEventListener('click',function(event){
-    event.preventDefault();
-    const donate = document.getElementById('card-1-input').value;
-    const donationCollected =document.getElementById('donation-collected').innerText;
-   
-    
-     if(donate>0 && donate != Text){
-        const balance = document.getElementById('account-balance').innerText;
-        const balanceNumber = parseFloat(balance);
-        
-        
+document.getElementById('card-1').
+    addEventListener('click', function (event) {
+        event.preventDefault();
+        const donate = document.getElementById('card-1-input').value;
+        const donationCollected = document.getElementById('donation-collected').innerText;
 
-        const donateNumber =parseFloat(donate);
-   
-     const totalDonation= parseFloat(donationCollected);
-    
-     const totalDonationCollected = totalDonation+donateNumber;
-     document.getElementById('donation-collected').innerText=totalDonationCollected;
 
-     const totalBalance = balanceNumber-donateNumber;
-        document.getElementById('account-balance').innerText= totalBalance;
-     }  
-     else{
-        
-        alert (`Invalid Donation amount!
+        if (donate > 0 && donate != Text) {
+            const balance = document.getElementById('account-balance').innerText;
+            const balanceNumber = parseFloat(balance);
+
+
+
+            const donateNumber = parseFloat(donate);
+
+            const totalDonation = parseFloat(donationCollected);
+
+            const totalDonationCollected = totalDonation + donateNumber;
+            document.getElementById('donation-collected').innerText = totalDonationCollected;
+
+            const totalBalance = balanceNumber - donateNumber;
+            document.getElementById('account-balance').innerText = totalBalance;
+
+            // add transiction
+
+            const p=document.createElement('p');
+            p.innerText = `Donate:${donateNumber} tk.Balance:${totalBalance}`;
+            console.log(p);
+
+
+            document.getElementById('transaction-container').appendChild(p);
+        }
+        else {
+
+            alert(`Invalid Donation amount!
 
         Please Enter a valid amount.`);
-     }
-})
+        }
+    })
 
 // card-2
